7404bec02db4cd384578173ba8925544dac8d44c
xiaomingtongxie <xmtx.2015@gmail.com>
2018-12-02 11:09:36 +0800

    micro zookeeper demos

---
+++ micro-zookeeper/src/main/java/com.example/ZookeeperApplication.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ZookeeperApplication {
+
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ZookeeperApplication.class,args);
+
+    }
+
+
+}
